f849a47117f97c71d10a5391734a239f47b7b737
Josue Henrique <josue21henrique@gmail.com>
2025-03-11 09:57:05 -0300

    configurando o ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-    apps: [
-      {
-        name: "API-DIARIAAQUI",
-        script: "ts-node src/server.ts",
-        instances: 1,
-        exec_mode: "fork",
-        env: {
-          NODE_ENV: "production",
-          PORT: 3001
-        }
-      }
-    ]
-  };
-  
+  apps: [
+    {
+      name: "API-DIARIAAQUI",
+      script: "./dist/server.js",
+      instances: 1,
+      interpreter: "node",
+      env: {
+        NODE_ENV: "development"
+      },
+      watch: true,
+      ignore_watch: ["node_modules", "logs"],
+      error_file: "logs/error.log",
+      out_file: "logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss"
+    }
+  ]
+};
